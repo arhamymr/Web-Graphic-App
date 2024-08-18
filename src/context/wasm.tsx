924b79c102c1e5d8a6ff7c450fd2b6134a3cb265
muhammad arham <arhamymr@gmail.com>
2024-08-18 14:25:11 +0700

    feat: handle error window

--- src/context/wasm.tsx
+++ src/context/wasm.tsx
@@ -12,40 +12,58 @@ const WasmContext = createContext<WasmContextProps | undefined>(undefined);
 export const WasmProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const SCREEN_WIDTH = window.innerWidth;
-  const SCREEN_HEIGHT = window.innerHeight;
-
+  const [isLoading, setLoading] = useState<boolean>(true);
   const [module, setModule] = useState<any>(null);
-
+  const [screen, setScreen] = useState<{ w: number; h: number }>({
+    w: 0,
+    h: 0,
+  });
   const loadWasm = async () => {
     try {
       const loadmodule = await import('@/wasm/main.js');
       const instance = await loadmodule.default();
       instance.canvas = document.getElementById('canvas');
-      const myModule = new instance.App(SCREEN_HEIGHT, SCREEN_WIDTH);
+      const myModule = new instance.App(screen.w, screen.h);
       myModule.mainLoop();
       setModule(myModule);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    loadWasm();
-  }, []);
+    if (typeof window !== 'undefined') {
+      setScreen({
+        w: window.innerWidth,
+        h: window.innerHeight,
+      });
+
+      loadWasm();
+    }
+  }, [screen.w, screen.h]);
 
   return (
-    <div className="bg-black w-full h-full">
-      <canvas
-        id="canvas"
-        className="w-full"
-        width={SCREEN_WIDTH}
-        height={SCREEN_HEIGHT}
-      ></canvas>
-      <WasmContext.Provider value={{ module, loadWasm }}>
-        {children}
-      </WasmContext.Provider>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="text-white">Loading...</div>
+        </div>
+      ) : (
+        <div className="bg-black w-full h-full">
+          <canvas
+            id="canvas"
+            className="w-full"
+            width={screen.w}
+            height={screen.h}
+          ></canvas>
+          <WasmContext.Provider value={{ module, loadWasm }}>
+            {children}
+          </WasmContext.Provider>
+        </div>
+      )}
+    </>
   );
 };
 
